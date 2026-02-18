7b6e8a5d94889d2f3455d9b9bfac0de96d2ab71d
Alejna1004 <alejnabunjaku12@gmail.com>
2026-02-18 20:48:51 +0100

    modul3

---
+++ modul_3/index.php
@@ -0,0 +1,48 @@
+<?php
+    //$num = -5;
+    //if ($num < 0){
+    //    echo "$num is less than 0";
+    //}
+
+    //$age = 15;
+    //if( $age < 18){
+    //    echo "You are under 18";
+    //}else{
+    //    echo "You are an adult!";
+    //}
+
+    //$age = 15;
+    //if(($age > 12) && ($age < 18)){
+    //    echo "You are a teenager";
+    //}
+
+    //$num = 5;
+    //if($num < 0){
+    //    echo "The value of $num is a negative number.";
+    //}elseif($num==0){
+    //    echo "The value of $num is 0.";
+    //}else{
+    //    echo "The value of $num is a positive number.";
+    //}
+
+    $a = 1;
+    $b = 1;
+
+    //if($a == $b){
+    //    echo "yes";
+    //}else{
+    //    echo "no";
+    //}
+
+    if($a == $b){
+        echo "1";
+    
+    }elseif($a > $b){
+        echo "2";
+    
+    }else($a < $b){
+        echo "3";
+    }
+
+
+?>

c537891157eafa831b18933f31d16537bb8bea56
Alejna1004 <alejnabunjaku12@gmail.com>
2026-02-25 20:48:18 +0100

    variables

---
+++ modul4/user_defined_functions.php
@@ -0,0 +1,24 @@
+<?php
+    function helloWorld(){
+        echo "Hello World";
+    }
+
+    helloWorld();
+
+    function sum($x, $y){
+        //return x + y;
+        $z = $x + $y;
+        return $z;
+    }
+    echo "Sum:" .sum(10, 30);
+
+    function maximum($x, $y){
+        if($x > $y){
+            return $x;
+        }else{
+            return $y;
+        }
+    }
+    $result = maximum(10, 15);
+    echo "The max is:".$result;
+?>

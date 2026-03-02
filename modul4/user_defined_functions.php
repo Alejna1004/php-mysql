a52554c9ff55468cf14b4a07c759034f133cff1a
Alejna1004 <alejnabunjaku12@gmail.com>
2026-03-02 20:50:51 +0100

    arrarys

--- modul4/user_defined_functions.php
+++ modul4/user_defined_functions.php
@@ -21,4 +21,17 @@
     }
     $result = maximum(10, 15);
     echo "The max is:".$result;
+
+    function fully_divisible($n){
+        if(($n % 2) == 0){
+            return "$n is fully divisible by 2";
+        }else{
+            return "$n is not fully divisible by 2";
+        }
+    }
+
+    print_r(fully_divisible(4). "<br>");
+    print_r(fully_divisible(35). "<br>");
+    print_r(fully_divisible(14). "<br>");
+    print_r(fully_divisible(15). "<br>");
 ?>

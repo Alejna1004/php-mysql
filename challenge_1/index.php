f3d1fdccf87c16b63694c7a2abdc83021a8bfa96
Alejna1004 <alejnabunjaku12@gmail.com>
2026-04-15 20:44:46 +0200

    challenge

---
+++ challenge_1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Challenge no.1</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="id" placeholder="Enter your id"><br>
+        <input type="text" name="name" placeholder="Enter your name"><br>
+        <input type="email" name="email" placeholder="Enter your email"><br>
+        <input type="text" name="age" placeholder="Enter your age"><br>
+        <button type="submit" name="submit">Submit</button>
+    </form>
+</body>
+</html>

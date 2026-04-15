74c5c7af5ceb0dd7b2045f3c983c056373eb4db5
Alejna1004 <alejnabunjaku12@gmail.com>
2026-04-15 20:44:18 +0200

    modul12

---
+++ modul12/pages/login.php
@@ -0,0 +1,17 @@
+<?php include '../includes/header.php'; ?>
+
+<div class="container mt-5">
+    <h2>Login</h2>
+    <form action="">
+        <div>
+            <label for="Email:"></label>
+            <input type="email" class="form-control" placeholder="Enter email">
+        </div>
+        <div>
+            <label for="Password:"></label>
+            <input type="password" class="form-control mb-3" placeholder="Enter password">
+        </div>
+        <button class="btn btn-primary mb-3">Login</button>
+    </form>
+</div>
+<?php include '../includes/footer.php'; ?>
---
+++ modul12/pages/signup.php
@@ -0,0 +1,14 @@
+<?php include '../includes/header.php'; ?>
+
+<div class="container" mt-5>
+    <h2>Sign Up</h2>
+    <form action="">
+        <input type="text" class="form-control mb-3" placeholder="Full name">
+        <input type="email" class="form-control mb-3" placeholder="Email">
+        <input type="password" class="form-control mb-3" placeholder="Password">
+        <input type="password" class="form-control mb-3" placeholder="Confirm password">
+        
+        <button class="btn btn-success">Register</button>
+    </form>
+</div>
+<?php include '../includes/footer.php'; ?>
